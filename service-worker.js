7e0aa355ad7ae82a55b4a8dcd1a929881d849386
Sergei <serheifil@gmail.com>
2021-07-30 17:55:48 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.2adeca5491144defb4cf34651e903db0.js"
 );
 
 self.addEventListener('message', (event) => {
